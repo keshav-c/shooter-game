e324b5e481975ec6ca6c1e2519f165f795991237
keshav-c <keshav1985@gmail.com>
2020-09-10 21:51:33 +0530

    Allow player to shoot lasers

--- src/js/Player.js
+++ src/js/Player.js
@@ -1,11 +1,15 @@
 import Phaser from 'phaser';
 import Entity from './Entity';
+import PlayerLaser from './PlayerLaser';
 
 class Player extends Entity {
   constructor(scene, x, y, key) {
     super(scene, x, y, key, 'Player');
     this.setData('speed', 200);
     this.play('sprPlayer');
+    this.setData('isShooting', false);
+    this.setData('timerShootDelay', 10);
+    this.setData('timerShootTick', this.getData('timerShootDelay') - 1);
   }
 
   moveUp() {
@@ -28,6 +32,19 @@ class Player extends Entity {
     this.body.setVelocity(0, 0);
     this.x = Phaser.Math.Clamp(this.x, 0, this.scene.game.config.width);
     this.y = Phaser.Math.Clamp(this.y, 0, this.scene.game.config.height);
+
+    // Player shooting logic
+
+    if (this.getData('isShooting')) {
+      if (this.getData('timerShootTick') < this.getData('timerShootDelay')) {
+        this.setData('timerShootTick', this.getData('timerShootTick') + 1);
+      } else {
+        const laser = new PlayerLaser(this.scene, this.x, this.y);
+        this.scene.playerLasers.add(laser);
+        this.scene.sfx.laser.play();
+        this.setData('timerShootTick', 0);
+      }
+    }
   }
 }
 
--- src/js/SceneMain.js
+++ src/js/SceneMain.js
@@ -186,6 +186,13 @@ class SceneMain extends Phaser.Scene {
       this.player.moveRight();
     }
 
+    if (this.keySpace.isDown) {
+      this.player.setData('isShooting', true);
+    } else {
+      this.player.setData('timerShootTick', this.player.getData('timerShootDelay') - 1);
+      this.player.setData('isShooting', false);
+    }
+
     for (let i = 0; i < this.enemies.getChildren().length; i += 1) {
       const enemy = this.enemies.getChildren()[i];
       enemy.update();
