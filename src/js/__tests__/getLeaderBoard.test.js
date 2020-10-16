a7d1b36495d1c1d24042c75238a5ade4ad4a41cf
keshav-c <keshav1985@gmail.com>
2020-10-16 16:51:19 +0530

    Add zero score testing

--- src/js/__tests__/getLeaderBoard.test.js
+++ src/js/__tests__/getLeaderBoard.test.js
@@ -10,3 +10,9 @@ it('gets the sorted leader board', () => {
   return getLeaderBoard('abc', 123)
     .then(data => expect(data).toEqual(fakeLeaderBoard));
 });
+
+it('doesn\'t accept games with 0 score', () => {
+  expect.assertions(1);
+  return getLeaderBoard('abc', 0)
+    .catch(reason => expect(reason).toEqual(new Error('invalid score')));
+});
